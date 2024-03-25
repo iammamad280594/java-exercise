5a76290a1d3a7182e337a43ed66647d52e24d7a6
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 09:04:40 +0200

    Update Exercise6.java
    nur muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,17 +1,49 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise6 {
-    /**
-     * Java Array Program to Remove All Occurrences of an Element in an Array
-     *
-     * Input: array = [ 1, 2, 1, 3, 5, 1 ] , key = 1
-     * Output: [2, 3, 5]
-     *
-     * Explanation: all the occurrences of element 1 is removed from the array So, array becomes from
-     * [ 1, 2, 1, 3, 5, 1 ]  to
-     * Final result: [2, 3, 5]
-     */
     public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 5, 1};
+        int elementToRemove = 1;
 
+        System.out.println("array awal:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+
+        arr = removeAllOccurrences(arr, elementToRemove);
+
+
+        System.out.println("setelah dihapus " + elementToRemove + ":");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] removeAllOccurrences(int[] arr, int element) {
+        int count = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                count++;
+            }
+        }
+
+
+        int[] newArray = new int[arr.length - count];
+        int index = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != element) {
+                newArray[index++] = arr[i];
+            }
+        }
+
+        return newArray;
     }
 }
+
+

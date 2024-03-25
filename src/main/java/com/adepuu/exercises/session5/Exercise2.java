3adb9198c25a40a053fc9792a8f5393b6a217adb
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 08:00:41 +0200

    Update Exercise2.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,14 +1,31 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise2 {
-    /**
-     * Java Array Program For Array Rotation
-     * Input: arr[] = {1, 2, 3, 4, 5, 6, 7},  d = 2
-     * Output:  3 4 5 6 7 1 2
-     * Explanation: d=2 so 2 elements are rotated to the end of the array. So, 1 2 is rotated back
-     * So, Final result: 3, 4, 5, 6, 7, 1, 2
-     */
     public static void main(String[] args) {
+            int[] arr = {1,2,3,4,5,6,7};
+            int rotateby = 2;
 
+            int[] rotatedArr = rotateArray(arr, rotateby);
+
+            for (int num : rotatedArr) {
+                System.out.print(num );
+
+            }}
+
+    }
+    public static int[] rotateArray(int[] arr, int rotateBy){
+        int len = arr.length;
+        int[] rotatedArr = new int[len];
+
+        for (int i = 0; i < len - rotateBy; i++) {
+            rotatedArr[i] = arr[i + rotateBy];
+
+        }
+
+        for (int i = 0; i < rotateBy; i++) {
+            rotatedArr[len - rotateBy + i] = arr[i];
+        }
+
+        return rotatedArr;
     }
 }

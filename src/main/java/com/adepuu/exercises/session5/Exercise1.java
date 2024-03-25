c9183770c8060d116a486bfdc9914341ad6ba22b
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 07:42:19 +0200

    Update Exercise1.java
    nur muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,32 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+  public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int rotateBy = 2; 
+
+  }
+  {
+        int[] rotatedArr = rotateArray(arr, rotateBy);
+
+        for (int num : rotatedArr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] rotateArray(int[] arr, int rotateBy) {
+        int len = arr.length;
+        int[] rotatedArr = new int[len];
+
+        for (int i = 0; i < len - rotateBy; i++) {
+            rotatedArr[i] = arr[i + rotateBy];
+        }
+
+        for (int i = 0; i < rotateBy; i++) {
+            rotatedArr[len - rotateBy + i] = arr[i];
+        }
+        
+        return rotatedArr;
+    }
+}
 }

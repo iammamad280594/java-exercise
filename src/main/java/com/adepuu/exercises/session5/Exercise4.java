6127254953f8c9b73eb5323cde44829b051a9890
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 08:27:37 +0200

    Update Exercise4.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,14 +1,48 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise4 {
-    /**
-     * Java Array Program to Remove Duplicate Elements From an Array
-     *
-     * Input: [ 1, 2, 2, 3, 3, 3, 4, 5 ]
-     * Output: [ 1, 2, 3, 4, 5 ]
-     * @param args
-     */
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 5, 1};
+        int elementToRemove = 1;
 
+        System.out.println("array awal:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+
+        arr = removeAllOccurrences(arr, elementToRemove);
+
+
+        System.out.println("setelah dihapus " + elementToRemove + ":");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] removeAllOccurrences(int[] arr, int element) {
+        int count = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                count++;
+            }
+        }
+
+
+        int[] newArray = new int[arr.length - count];
+        int index = 0;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != element) {
+                newArray[index++] = arr[i];
+            }
+        }
+
+        return newArray;
     }
 }
+

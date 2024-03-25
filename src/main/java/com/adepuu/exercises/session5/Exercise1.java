4c7f9f406e3e164b9ef360bf294312860d9cc2c1
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 08:02:22 +0200

    Update Exercise1.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,12 @@
 package com.adepuu.exercises.session5;
-
+import java.util.Arrays;
 public class Exercise1 {
+  ublic class LargestElementOfArray {
+    public static void main(String[] args) {
+        int[] array1 = {7,2,5,1,4};
+        Arrays.sort(array1);
+        int largest = array1[array1.length-1];
+        System.out.println("nilai paling gede adalah: " +largest);
+    }
+
 }

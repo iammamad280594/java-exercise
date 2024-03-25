d520686db577edabb8089dd023e2ec70fbb55be7
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 08:03:52 +0200

    Update Exercise2.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,14 +1,39 @@
 package com.adepuu.exercises.session5;
-
+import java.util.Arrays;
 public class Exercise2 {
-    /**
-     * Java Array Program For Array Rotation
-     * Input: arr[] = {1, 2, 3, 4, 5, 6, 7},  d = 2
-     * Output:  3 4 5 6 7 1 2
-     * Explanation: d=2 so 2 elements are rotated to the end of the array. So, 1 2 is rotated back
-     * So, Final result: 3, 4, 5, 6, 7, 1, 2
-     */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        int[] array1 = {7,2,5,1,4};
+        Arrays.sort(array1);
+        int largest = array1[array1.length-1];
+        System.out.println("nilai paling gede adalah: " +largest);
+
+     {
+            int[] arr = {1,2,3,4,5,6,7};
+            int rotateby = 2;
+
+            int[] rotatedArr = rotateArray(arr, rotateby);
+
+            for (int num : rotatedArr) {
+                System.out.print(num );
+
+            }}
 
     }
+    public static int[] rotateArray(int[] arr, int rotateBy){
+        int len = arr.length;
+        int[] rotatedArr = new int[len];
+
+        for (int i = 0; i < len - rotateBy; i++) {
+            rotatedArr[i] = arr[i + rotateBy];
+
+        }
+
+        for (int i = 0; i < rotateBy; i++) {
+            rotatedArr[len - rotateBy + i] = arr[i];
+        }
+
+        return rotatedArr;
+    }
+}
+
 }

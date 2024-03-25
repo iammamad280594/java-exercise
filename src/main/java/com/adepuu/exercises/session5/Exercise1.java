3f569f71b0a2c6d4df8ce05f6840af1a0da5cf19
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 06:46:49 +0200

    Update Exercise1.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,34 @@
 package com.adepuu.exercises.session5;
-
+import java.util.Arrays;
 public class Exercise1 {
+
+    public static void main(String[] args) {
+        int[] array1 = {7,2,5,1,4};
+        Arrays.sort(array1);
+        int largest = array1[array1.length-1];
+        System.out.println("nilai paling gede adalah: " +largest);
+
+     {
+            int[] array2 = {1,2,3,4,5,6,7};
+            int rotationSteps = 2;
+
+            System.out.println("original array: " + Arrays.toString(array2));
+            rotateArray(array2, rotationSteps);
+
+    }
+
+
+    }
+    public static void rotateArray(int[] array, int steps){
+        int length = array.length;
+        steps = steps % length;
+        int[] tempArray = new int[steps];
+        System.arraycopy(array, 0, array, steps, length-steps);
+        System.arraycopy(tempArray,0, array,0, steps);
+    }
+}
+
+
+
+
 }

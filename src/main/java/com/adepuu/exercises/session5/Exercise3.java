dbb10e3c92ece7caa71640f3c1004aa39f0d82c6
iammamad280594 <152339048+iammamad280594@users.noreply.github.com>
2024-03-25 07:56:26 +0200

    Update Exercise3.java
    Nur Muhammad

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -1,22 +1,32 @@
 package com.adepuu.exercises.session5;
-
+import java.util.HashSet;
+import java.util.Set;
 public class Exercise3 {
-    /**
-     * Write a Java Program to Check if Array Contain Duplicates
-     *
-     * Example 1:
-     * Input: nums = [1,2,3,1]
-     * Output: true
-     *
-     * Example 2:
-     * Input: nums = [1,2,3,4]
-     * Output: false
-     *
-     * Example 3:
-     * Input: nums = [1,1,1,3,3,4,3,2,4,2]
-     * Output: true
-     */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        int[] arr1 = {1,2,3,1};
+        int[] arr2 = {1,2,3,4};
+        int[] arr3 = {1,1,1,3,3,4,3,2,4,2};
+
+        System.out.println("ini duplikat? " + containsDuplicates(arr1));
+        System.out.println("ini duplikat? " + containsDuplicates(arr2));
+        System.out.println("ini duplikat? " + containsDuplicates(arr3));
+    }
+
+    public static boolean containsDuplicates(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(num)) {
+                return true; // Duplicate found
+            } else {
+                set.add(num);
+            }
+        }
+
+        return false; // No duplicates found
+    }
+}
+
 
     }
 }
